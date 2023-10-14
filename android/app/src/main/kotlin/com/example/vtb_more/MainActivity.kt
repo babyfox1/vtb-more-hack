fa5ef03d79e9b071fac3f887b0d9aa437f82d0a1
Роман Савельев <r.zrk@yandex.ru>
2023-10-14 14:51:51 +0500

    Приложение открывается и сразу закрывается

--- android/app/src/main/kotlin/com/example/vtb_more/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/vtb_more/MainActivity.kt
@@ -1,6 +1,16 @@
 package com.example.vtb_more
 
+import com.yandex.mapkit.MapKitFactory
+import androidx.annotation.NonNull
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
 
-class MainActivity: FlutterActivity() {
+class MainActivity : FlutterActivity() {
+
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine){
+        GeneratedPluginRegistrant.registerWith(flutterEngine)
+        MapKitFactory.setApiKey("b3a03827-e8c5-46dc-806a-a767fc032d23")
+    }
+
 }
